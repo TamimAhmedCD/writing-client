db1420116eee8ac4b4d669a47e922052b21e02de
Tamim Ahmed <tamim20072@gmail.com>
2024-12-24 20:29:10 +0600

    condition by author email

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -13,6 +13,7 @@ const Comments = ({ blogs }) => {
   const userPhoto = user.photoURL;
   const [loading, setLoading] = useState(true);
   const [comments, setComments] = useState([]);
+  const authorEmail = blogs.userEmail;
 
   const toggleComments = () => {
     setToggleComment((prev) => !prev);
@@ -43,7 +44,9 @@ const Comments = ({ blogs }) => {
         })
         .then((response) => {
           if (response.data) {
+            setComments((prevComments) => [...prevComments, newComment]);
             toast.success("Successfully post your Comment");
+            e.target.reset();
           }
         });
     }
@@ -130,76 +133,82 @@ const Comments = ({ blogs }) => {
           )}
 
           {/* Post Comment Form */}
-          <form
-            className="space-y-4 mt-8 bg-light-accent bg-opacity-5 p-5 rounded-xl"
-            onSubmit={handlePostComment}
-          >
-            <div className="form-control flex flex-row w-full gap-5">
-              <div className="w-full">
-                <label className="label" htmlFor="account">
-                  <span className="label-text">Name *</span>
-                </label>
-                <input
-                  type="text"
-                  name="userName"
-                  id="account"
-                  value={user.displayName}
-                  readOnly
-                  required
-                  className="input input-bordered w-full bg-transparent"
-                  placeholder="Name *"
-                />
+          {user.email === authorEmail ? (
+            <p className="text-red-500 font-medium">
+              Cannot comment on your own blog.
+            </p>
+          ) : (
+            <form
+              className="space-y-4 mt-8 bg-light-accent bg-opacity-5 p-5 rounded-xl"
+              onSubmit={handlePostComment}
+            >
+              <div className="form-control flex flex-row w-full gap-5">
+                <div className="w-full">
+                  <label className="label" htmlFor="account">
+                    <span className="label-text">Name *</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="userName"
+                    id="account"
+                    value={user.displayName}
+                    readOnly
+                    required
+                    className="input input-bordered w-full bg-transparent"
+                    placeholder="Name *"
+                  />
+                </div>
+                <div className="w-full">
+                  <label className="label" htmlFor="name">
+                    <span className="label-text">Email *</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="userEmail"
+                    value={user.email}
+                    readOnly
+                    id="name"
+                    className="input input-bordered w-full bg-transparent"
+                    required
+                  />
+                </div>
               </div>
-              <div className="w-full">
-                <label className="label" htmlFor="name">
-                  <span className="label-text">Email *</span>
+              <div className="form-control">
+                <label className="label" htmlFor="longDes">
+                  <span className="label-text">Your Comment *</span>
                 </label>
-                <input
-                  type="text"
-                  name="userEmail"
-                  value={user.email}
-                  readOnly
-                  id="name"
-                  className="input input-bordered w-full bg-transparent"
+                <textarea
+                  className="textarea textarea-bordered h-28 bg-transparent"
+                  placeholder="Your Comment *"
+                  id="longDes"
+                  name="commentDes"
                   required
-                />
+                ></textarea>
               </div>
-            </div>
-            <div className="form-control">
-              <label className="label" htmlFor="longDes">
-                <span className="label-text">Your Comment *</span>
-              </label>
-              <textarea
-                className="textarea textarea-bordered h-28 bg-transparent"
-                placeholder="Your Comment *"
-                id="longDes"
-                name="commentDes"
-                required
-              ></textarea>
-            </div>
-            <div className="form-control">
-              <label className="label cursor-pointer justify-start gap-3">
-                <input
-                  type="checkbox"
-                  defaultChecked
-                  className="checkbox [--chkbg:theme(colors.white)] [--chkfg:black]"
-                />
-                <span className="label-text text-light-primary-color">
-                  Save my name and email in this browser for the next time I
-                  comment.
-                </span>
-              </label>
-            </div>
-            <button>
-              {" "}
-              <Button
-                variant="gradient"
-                className="btn normal-case font-medium text-base bg-gradient-to-t from-[#514dcc] to-[#9895ff] hover:from-[#4440b4] hover:to-[#9895ff] shadow-none hover:shadow-[#9895ffa8] hover:border-none border-none w-full"
-              >
-                <span>Submit Comment</span>
-              </Button>
-            </button>
-          </form>
+              <div className="form-control">
+                <label className="label cursor-pointer justify-start gap-3">
+                  <input
+                    type="checkbox"
+                    defaultChecked
+                    className="checkbox [--chkbg:theme(colors.white)] [--chkfg:black]"
+                  />
+                  <span className="label-text text-light-primary-color">
+                    Save my name and email in this browser for the next time I
+                    comment.
+                  </span>
+                </label>
+              </div>
+              <button>
+                {" "}
+                <Button
+                  variant="gradient"
+                  className="btn normal-case font-medium text-base bg-gradient-to-t from-[#514dcc] to-[#9895ff] hover:from-[#4440b4] hover:to-[#9895ff] shadow-none hover:shadow-[#9895ffa8] hover:border-none border-none w-full"
+                >
+                  <span>Submit Comment</span>
+                </Button>
+              </button>
+            </form>
+          )}
         </div>
       )}
     </div>
